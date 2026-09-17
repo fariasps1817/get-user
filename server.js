3fda41bb5d803f43c43f852c99a6857f37fb6a3f
unknown <farias sousa>
2026-09-16 22:15:22 -0300

    feat: server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const http = require('node:http');
 
-const user = {
-    name: 'Farias João',
-    email: 'tes@example.com'
-};
+//const user = {
+//    name: 'Farias João',
+//    email: 'tes@example.com'
+//};
 
 http.createServer((request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
     response.writeHead(200, {'content-type': 'application/json'});
-    response.end(JSON.stringify(user));
+    response.end(JSON.stringify({
+        nome: 'farias joão',
+        email: 'farias.sousa@exemplo.com'
+    }));
 
 }).listen(3000);
